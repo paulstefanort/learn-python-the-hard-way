9c1ae6340666c80e8413c5c848be35f53f4ac239
Paul Stefan Ort <mail@paulstefanort.com>
2013-04-04 19:59:06 -0500

    Exercise 1 Drill 3 - Put a "#" (octothorpe) character at the beginning of a line. Add comments.

---
+++ ex1drill3.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+# This is commented. print "Hello Again"
+print "I like typing this."
+# This is also commented. print "This is fun."
+print 'Yay! Printing.'
+# The octothorpe character designates a comment. print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print 'This is another line.'
