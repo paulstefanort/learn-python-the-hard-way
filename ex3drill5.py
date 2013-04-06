e1781ffdfc27c186727103513db4643dbb95608b
Paul Stefan Ort <mail@paulstefanort.com>
2013-04-06 16:11:10 -0500

    Add Exercise 3 Drill 5.

---
+++ ex3drill5.py
@@ -0,0 +1,37 @@
+# output a line of text
+print "I will now count my chickens:"
+
+# output the result of a mathematical expression
+print "Hens", 25.0 + 30.0 / 6.0
+# output the result of a mathematical expression
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# output a line of text
+print "Now I will count the eggs:"
+
+# output the result of a mathematical expression
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# output a line of text
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# output the result of a mathematical expression
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# output a question and the result of a mathematical expression
+print "What is 3 + 2?", 3.0 + 2.0
+# output a question and the result of a mathematical expression
+print "What is 5 - 7?", 5.0 - 7.0
+
+# output a line of text
+print "Oh, that's why it's False."
+
+# output a line of text
+print "How about some more."
+
+# output a question and the result of a mathematical expression
+print "Is it greater?", 5.0 > -2.0
+# output a question and the result of a mathematical expression
+print "Is it greater or equal?", 5.0 >= -2.0
+# output a question and the result of a mathematical expression
+print "Is it less or equal?", 5.0 <= -2.0
